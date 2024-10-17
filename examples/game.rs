705f9a360250d8c9700e56933acbbde23c5540da
Matt Hunzinger <matthunz2@gmail.com>
2024-10-17 18:47:55 -0400

    Fix rotation

--- examples/game.rs
+++ examples/game.rs
@@ -36,11 +36,10 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
 fn rotate(models_query: Query<&VoxFileModels>, mut transform_query: Query<&mut Transform>) {
     for models in &models_query {
-        /*if let Some(entity) = models.entities.get("left_arm") {
-            transform_query
-                .get_mut(*entity)
-                .unwrap()
-                .rotate_around(Vec3::ZERO, Quat::from_rotation_x(0.01));
-        }*/
+        if let Some(entity) = models.entities.get("right_arm") {
+            let mut transform = transform_query.get_mut(*entity).unwrap();
+
+            transform.rotate_around(Vec3::new(0., 24., 4.), Quat::from_rotation_x(0.01));
+        }
     }
 }
